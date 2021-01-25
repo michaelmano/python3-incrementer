75ff8676a1b26341342254771b0621cd40ceeb19
Michael Mano <michael.mano26@gmail.com>
2021-01-25 15:05:24 +1000

    Initial Commit

---
+++ main.py
@@ -0,0 +1,38 @@
+import keyboard
+
+filename = "codes.txt"
+COUNT = 0
+
+def incrementCode():
+    global COUNT
+    COUNT = COUNT+1
+
+def resetCode():
+    global COUNT
+    COUNT = 0
+
+def main():
+    with open(filename) as codes:
+        for code in codes:
+            code = code.strip('\n')
+            code = code.strip('\t')
+            print(code)
+
+def printCode():
+    with open(filename) as fp:
+        for i, code in enumerate(fp):
+            if i == COUNT:
+                code = code.strip('\n')
+                code = code.strip('\t')
+                print('Attempting code', code)
+                keyboard.write(code)
+                if 'str' in code:
+                    break
+                break
+
+    incrementCode()
+
+keyboard.add_hotkey('cmd+.', printCode)
+keyboard.add_hotkey('cmd+]', resetCode)
+
+keyboard.wait('ctrl+shift+0')
